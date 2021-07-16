09299d312efc872db8c5c0fa92e33cec837703e2
Hasan Bakırcı <hasan.bakrci@gmail.com>
2021-07-16 19:04:04 +0300

    mongodb sifreli giris icin ayarlandı

--- Settings/MongoDbSettings.cs
+++ Settings/MongoDbSettings.cs
@@ -4,6 +4,13 @@ namespace catalog.Settings
     {
         public string Host { get; set; }
         public int Port { get; set; }
-        public string ConnectionString { get{return $"mongodb://{Host}:{Port}";} }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public string ConnectionString { get{return $"mongodb://{User}:{Password}@{Host}:{Port}";} }
     }
 }
+
+// Mongodb şifreli ise appsetting e username yazılır
+// dotnet user-secrets init
+// dotnet user-secrets set MongoDbSettings:Password mongopass
+//
